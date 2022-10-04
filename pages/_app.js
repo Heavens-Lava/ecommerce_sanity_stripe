abeaa29da5839f369a27b69ba4182b2e8aa1ee02
Jeffrey Macy <macy.jeffreyj@gmail.com>
2022-10-03 19:41:07 -0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import React from "react";
+import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      {/* layout component */}
+      <Layout>
+        <Toaster />
+        {/* main component inside layout component */}
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
